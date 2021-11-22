e0aa190f57680daec30a113ebd9c5c570efa61cb
Yossi Meshulam <73947067+yossimeshulam@users.noreply.github.com>
2021-11-22 22:40:25 +0200

    the real working version which pass the test

--- pubsub.h
+++ pubsub.h
@@ -15,6 +15,8 @@ int my_open(struct inode *, struct file *);
 
 int my_release(struct inode *, struct file *);
 
+ssize_t my_write(struct file *, const char *, size_t, loff_t *);
+
 ssize_t my_read(struct file *, char *, size_t, loff_t *);
 
 int my_ioctl(struct inode *inode, struct file *filp, unsigned int cmd, unsigned long arg);
